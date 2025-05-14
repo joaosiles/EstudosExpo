2be9748d3062186d046846a5b2a295165bb2926c
joaosiles <joaosiles@gmail.com>
2025-05-13 23:41:05 -0300

    Coisas

--- Expo-api/app/index.tsx
+++ Expo-api/app/index.tsx
@@ -1,15 +1,53 @@
-import { Text, View } from "react-native";
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
 
-export default function Index() {
+const firebaseConfig = {
+  apiKey: process.env.EXPO_PUBLIC_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_APP_ID
+};
+
+
+firebase.initializeApp(firebaseConfig);
+
+
+import React, { useEffect, useState } from 'react';
+import { View, Text, FlatList } from 'react-native';
+
+export default function App() {
+  const [nomes, setNomes] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const nomesCollection = firebase.firestore().collection('Nomes');
+      const snapshot = await nomesCollection.get();
+
+      const data = [];
+      snapshot.forEach((doc) => {
+        data.push({ id: doc.id, ...doc.data() });
+      });
+
+      setNomes(data);
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Lista de Nomes:</Text>
+      <FlatList
+        data={nomes}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <View>
+            <Text>{item.Nome} {item.Sobrenome}</Text>
+          </View>
+        )}
+      />
     </View>
   );
 }
