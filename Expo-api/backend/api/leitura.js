8d97658377921a1bafc8d004e68df839a2a0d10b
joaosiles <joaosiles@gmail.com>
2025-05-21 17:00:51 -0300

    Ajustes na estrutura do backend

---
+++ Expo-api/backend/api/leitura.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const firebase = require('../firebase-admin-connect');
+const router = express.Router();
+
+router.get('/leitura', async (req, res) => {
+  try {
+    const db = firebase.firestore();
+    const nomesCollection = db.collection('Nomes');
+    const snapshot = await nomesCollection.get();    
+
+    const data = [];
+    snapshot.forEach((doc) => {
+      data.push({ id: doc.id, ...doc.data() });
+    });
+    console.log('bateu');
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ error: 'Erro ao buscar dados', details: error.message });
+  }
+});
+
+module.exports = router;
--- Expo-api/backend/server.js
+++ Expo-api/backend/server.js
@@ -1,25 +1,15 @@
 const express = require('express');
 const firebase = require('./firebase-admin-connect');
+const cors = require('cors');
 
 const app = express();
 const port = 3000;
+app.use(cors());
+app.use(express.json());
 
-app.get('/leitura', async (req, res) => {
-  try {
-    const db = firebase.firestore();
-    const nomesCollection = db.collection('Nomes');
-    const snapshot = await nomesCollection.get();    
-
-    const data = [];
-    snapshot.forEach((doc) => {
-      data.push({ id: doc.id, ...doc.data() });
-    });
-    console.log('bateu');
-    res.status(200).json(data);
-  } catch (error) {
-    res.status(500).json({ error: 'Erro ao buscar dados', details: error.message });
-  }
-});
+//importando as rotas
+const leituraRoute = require('./api/leitura');
+app.use('/', leituraRoute);
 
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
