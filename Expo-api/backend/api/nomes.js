2926740972a133fa879b5052a6de304a84fcddec
joaosiles <joaosiles@gmail.com>
2025-05-24 23:34:51 -0300

    Ajustes na estrutura dos endpoints

--- Expo-api/backend/api/nomes.js
+++ Expo-api/backend/api/nomes.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const firebase = require('../firebase-admin-connect');
 const router = express.Router();
+router.use(express.json());
+router.use(express.urlencoded({ extended: true }))
 
 router.get('/nomes', async (req, res) => {
   try {
@@ -11,16 +13,45 @@ router.get('/nomes', async (req, res) => {
     const data = [];
     snapshot.forEach((doc) => {
       data.push({ id: doc.id, ...doc.data() });
+    });    
+    res.status(200).json({
+      data,
+      message: "Success"
     });
-    console.log('bateu');
-    res.status(200).json(data);
   } catch (error) {
-    res.status(500).json({ error: 'Erro ao buscar dados', details: error.message });
+    res.status(500).json({
+      error: 'Erro ao buscar dados',
+      details: error.message
+    });
   }
 });
 
 router.post('/nomes', async (req, res) => {
-  res.send('POST nomes')
+  const nomesCollection = firebase.firestore().collection('Nomes');
+  
+  if (!req.body.nome || !req.body.sobrenome) {
+    return res.status(400).json({
+      error: "MissingFields",
+      message: "All request body fields are required."
+    }); 
+  }
+
+  try {
+    await nomesCollection.add({
+      Nome: req.body.nome,
+      Sobrenome: req.body.sobrenome
+    });    
+    res.status(200).json({
+      content: req.body, 
+      message: "Success"      
+    },);
+  } catch (error) {
+    console.error(error);
+    res.status(400).json({
+      content: req.body,
+      message: "Internal Server Error"
+    });
+  }
 });
 
 router.delete('/nomes', async (req, res) => {
